68bcc77eac90e3ed38c78e9d663a0d61fafa1b49
russel-jacolbia <russeljacolbia36@gmail.com>
2024-12-23 11:26:49 +0800

    added home.blade.php

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TenantController;
 use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\TicketController;
+use App\Http\Controllers\HomeController;
 
 Route::resource('tenants', TenantController::class);
 Route::resource('payments', PaymentController::class);
 Route::resource('tickets', TicketController::class);
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
+// Route::get('/', function () {
+//     return view('welcome');
+// });
